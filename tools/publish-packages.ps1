9c0de2f35c2e99066172fbaa6cda1dd3027a0a4e
Naves Harlen <harlen.naves@volvo.com>
2020-01-14 20:37:52 -0300

    Added required tools to automate build

---
+++ tools/publish-packages.ps1
@@ -0,0 +1,10 @@
+Param(
+    [Parameter(Mandatory=$true)][string]$apiKey
+)
+
+$packages = Get-ChildItem -Filter *.nupkg -Recurse -File -Name | ForEach-Object { [System.IO.Path]::GetFullPath($_) }
+
+ForEach ($package in $packages) {
+    Write-Host $package
+    #nuget push -SkipDuplicate -ApiKey $apiKey -Source https://api.nuget.org/v3/index.json $package
+}
